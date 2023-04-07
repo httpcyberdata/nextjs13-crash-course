ce64d5aa6604c86781c29f727f4522c7b26017d4
httpcyberdata <shieldedsafety@gmail.com>
2023-04-07 14:23:56 -0400

    chore: produced async fetchRepos() to fetch data from GitHub API & imported react-icons & mapped through array of repos

--- app/code/repos/page.jsx
+++ app/code/repos/page.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import Link from 'next/link';
+import fetch from 'isomorphic-fetch';
+import { FaStar, FaCodeBranch, FaEye } from 'react-icons/fa';
+async function fetchRepos() {
+    const response = await fetch('https://api.github.com/users/httpcyberdata/repos');
+    const repos = await response.json();
+    return repos;
+}
+const ReposPage = async () => {
+    const repos = await fetchRepos();
 
-const ReposPage = () => {
   return (
-    <div>ReposPage</div>
+    <div className="repos-container">
+        <h2>Repositories</h2>
+        <ul className="repo-list">
+            { repos.map((repo) => (
+                <li key={repo.id}>
+                    <Link href={`/code/repos/${repo.name}`}>
+                        <h3>{repo.name}</h3>
+                        <p>{repo.description}</p>
+                        <div className="repo-details">
+                            <span>
+                                <FaStar /> {repo.stargazers_count}
+                            </span>
+                            <span>
+                                <FaCodeBranch /> 
+                            </span>
+                            <span>
+                                <FaEye /> {repo.watchers_count}
+                            </span>
+                        </div>
+                    </Link>
+                </li>
+            ))}
+        </ul>
+    </div>
   )
 }
 
