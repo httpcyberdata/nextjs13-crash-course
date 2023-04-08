079c1858671da6fe00ccf32554aef4692a89f517
httpcyberdata <shieldedsafety@gmail.com>
2023-04-07 23:25:35 -0400

    docs: update README

---
+++ README.md
@@ -0,0 +1,21 @@
+# NextJS Crash Course
+Tutorial URL: https://www.youtube.com/watch?v=Y6KDk5iyrYE
+Tutorial description: A complete crash course to NextJS version 13 and it's new features such as the app directory structure, routing, React Server Components vs client components, layouts and more. 
+
+___________
+
+### Software versions
+
+### Problems in the project:
+1.
+2.
+
+### How to use
+
+### Photos of project:
+
+### Apps used:
+
+### Coding styles I used:
+
+### What I learned
---
+++ app/about/team/page.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const TeamPage = () => {
+  return (
+    <div>
+        <h1>Our Team</h1>
+        <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+            Natus quis, aliquam voluptatum reiciendis aspernatur delectus
+            perspiciatis obcaecati? Aliquid at voluptatibus architecto dicta 
+            soluta animi commodi, inventore unde consequuntur, perspiciatis nulla!
+         </p>
+    </div>
+  )
+}
+
+export default TeamPage;
